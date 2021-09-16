843260b5e4081baf947e1ebc4756a054d4cbf6a6
kencinas95 <encinaskevin096@gmail.com>
2021-09-16 18:19:13 -0300

    Upgrade version to 0.0.4

--- setup.py
+++ setup.py
@@ -1,6 +1,18 @@
 from distutils.core import setup
 
+
+with open('README.md', 'r') as md:
+    long_description = md.read()
+
 setup(
+  name='py-django-health',
+  version='0.0.4',
+  author='kencinas95',
+  author_email='kencinas95@developers.com',
+  description='Health checkers for Django apps',
+  long_description=long_description,
+  url='url = https://github.com/kencinas95/py-django-health',
+  license='GPLv3',
   packages=['health', 'health.checkers'],
   keywords=['health', 'checkers', 'heartbeat', 'microservices'],
   install_requires=[
